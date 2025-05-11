7bed0ec7dfcc12d01e2919b25116a41ec643e516
Ruben Fábio Gutierrez Gongora <ruben.fabio2016@gmail.com>
2025-05-10 21:25:07 -0300

    feat: add health check endpoint and start HTTP server
    Introduce a health check handler at the root endpoint ("/") to verify server status. Start an HTTP server on port 8080 to handle incoming requests. This change enables basic server functionality and provides a way to monitor server health.

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+// handler de health check
+func healthHandler(w http.ResponseWriter, r *http.Request) {
+    w.Write([]byte("gopher-tasks is running!"))
+}
 
 func main() {
-    fmt.Println("Hello, gopher-tasks!")
+    // rota raiz
+    http.HandleFunc("/", healthHandler)
+
+    addr := ":8080"
+    fmt.Println("Starting server on", addr)
+    if err := http.ListenAndServe(addr, nil); err != nil {
+        log.Fatalf("server failed: %v", err)
+    }
 }
